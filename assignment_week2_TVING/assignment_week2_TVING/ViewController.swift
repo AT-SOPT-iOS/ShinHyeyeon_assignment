6ff2ef4d0d2e8aa1a0706fddfeb4a372f5dd1ad9
hyeyeonie <shy020916@naver.com>
2025-04-25 21:32:47 +0900

    docs: .gitignore 추가

---
+++ assignment_week2_TVING/assignment_week2_TVING/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment_week2_TVING
+//
+//  Created by 신혜연 on 4/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
