9a65a23cc877fa38797866b148616171e958e243
hyeyeonie <shy020916@naver.com>
2025-05-05 03:21:32 +0900

    feat: '실시간 인기 LIVE' UICollectionView 구현

--- assignment_week2_TVING/assignment_week2_TVING/Presentation/Main/Cell/RealTimePopularLiveCell.swift
+++ assignment_week2_TVING/assignment_week2_TVING/Presentation/Main/Cell/RealTimePopularLiveCell.swift
@@ -5,3 +5,95 @@
 //  Created by 신혜연 on 5/3/25.
 //
 
+import UIKit
+import Then
+
+class RealTimePopularLiveCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    static let identifier = "RealTimePopularLiveCell"
+    
+    private let liveImageView = UIImageView().then {
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 3
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    private let liveRankLabel = UILabel().then {
+        $0.font = UIFont.boldSystemFont(ofSize: 19)
+        $0.textColor = .white
+        $0.transform = CGAffineTransform(rotationAngle: 0.15)
+    }
+    
+    private let liveTitleLabel = UILabel().then {
+        $0.textColor = .white
+        $0.font = UIFont.systemFont(ofSize: 10)
+    }
+    
+    private let liveEpisodeLabel = UILabel().then {
+        $0.textColor = .gray
+        $0.font = UIFont.systemFont(ofSize: 10)
+    }
+    
+    private let liveRatingsLabel = UILabel().then {
+        $0.textColor = .gray
+        $0.font = UIFont.systemFont(ofSize: 10)
+    }
+    
+
+    
+    // MARK: - Life Cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Layout
+    private func setLayout() {
+        [liveImageView, liveRankLabel, liveTitleLabel, liveEpisodeLabel, liveRatingsLabel].forEach {
+            contentView.addSubview($0)
+        }
+        
+        liveImageView.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(8)
+            $0.height.equalTo(80)
+            $0.width.equalTo(160)
+        }
+        
+        liveRankLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(16)
+            $0.top.equalTo(liveImageView.snp.bottom).offset(8)
+        }
+        
+        liveTitleLabel.snp.makeConstraints {
+            $0.leading.equalTo(liveRankLabel.snp.trailing).offset(7)
+            $0.top.equalTo(liveImageView.snp.bottom).offset(10)
+        }
+        
+        liveEpisodeLabel.snp.makeConstraints {
+            $0.leading.equalTo(liveRankLabel.snp.trailing).offset(7)
+            $0.top.equalTo(liveTitleLabel.snp.bottom).offset(4)
+        }
+        
+        liveRatingsLabel.snp.makeConstraints {
+            $0.leading.equalTo(liveRankLabel.snp.trailing).offset(7)
+            $0.top.equalTo(liveEpisodeLabel.snp.bottom).offset(4)
+        }
+    }
+}
+
+extension RealTimePopularLiveCell {
+    func dataBind(_ itemData: RealTimePopularLiveContent, itemRow: Int) {
+        liveImageView.image = itemData.posterImage
+        liveRankLabel.text = "\(itemData.rank)"
+        liveTitleLabel.text = itemData.title
+        liveEpisodeLabel.text = itemData.episode
+        liveRatingsLabel.text = itemData.ratings
+
+    }
+}
--- assignment_week2_TVING/assignment_week2_TVING/Presentation/Main/View/RealTimePopularLive.swift
+++ assignment_week2_TVING/assignment_week2_TVING/Presentation/Main/View/RealTimePopularLive.swift
@@ -5,3 +5,104 @@
 //  Created by 신혜연 on 5/3/25.
 //
 
+import UIKit
+import SnapKit
+
+// UIView로 변경하기
+class RealTimePopularLive: UIView {
+    
+    // MARK: - Properties
+    private let itemData = RealTimePopularLiveContent.dummy()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "실시간 인기 LIVE"
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        return label
+    }()
+    
+    private let moreLabel: UILabel = {
+        let label = UILabel()
+        label.text = "더보기"
+        label.textColor = .gray
+        label.font = UIFont.systemFont(ofSize: 12)
+        return label
+    }()
+    
+    public let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 160, height: 140)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .black
+        collectionView.register(RealTimePopularLiveCell.self, forCellWithReuseIdentifier: RealTimePopularLiveCell.identifier)
+        return collectionView
+    }()
+    
+    // MARK: - Life Cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .black
+        
+        setLayout()
+        setDelegate()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Layout
+    private func setLayout() {
+        addSubview(titleLabel)
+        addSubview(moreLabel)
+        addSubview(collectionView)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().offset(8)
+        }
+        
+        moreLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.trailing.equalToSuperview().offset(-8)
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(9)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(140)
+        }
+    }
+    
+    
+    // MARK: - Delegate
+    private func setDelegate() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    private func setCollectionView() {
+        collectionView.register(TodaysTop20Cell.self, forCellWithReuseIdentifier: TodaysTop20Cell.identifier)
+    }
+}
+
+// MARK: - UICollectionView Delegate & DataSource
+extension RealTimePopularLive: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RealTimePopularLiveCell.identifier, for: indexPath) as? RealTimePopularLiveCell else {
+            return UICollectionViewCell()
+        }
+        cell.dataBind(itemData[indexPath.item], itemRow: indexPath.item)
+        return cell
+    }
+}
