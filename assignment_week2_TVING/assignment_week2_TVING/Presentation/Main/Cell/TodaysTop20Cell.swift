d89345533e7b363c663b5a0812f178f2af999c07
hyeyeonie <shy020916@naver.com>
2025-05-05 03:14:16 +0900

    Chore: 폴더 구조 및 파일 정리
    - 기존방식은 각 컬렉션에 해당하는 폴더를 만들어 내부에 MVC패턴으로 구현하였음
    - 셀과 UIView가 모두 분리되어 가독성이 떨어짐
    - 모든 컬렉션을 취합하는 메인 뷰컨트롤러의 부재
    - 위를 고려해 폴더 및 파일 구조 수정

--- assignment_week2_TVING/assignment_week2_TVING/Presentation/Main/Cell/TodaysTop20Cell.swift
+++ assignment_week2_TVING/assignment_week2_TVING/Presentation/Main/Cell/TodaysTop20Cell.swift
@@ -5,3 +5,60 @@
 //  Created by 신혜연 on 5/3/25.
 //
 
+import UIKit
+import Then
+
+class TodaysTop20Cell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    static let identifier = "TodaysTop20Cell"
+    
+    private let todayTopLabel = UILabel().then {
+        $0.textColor = .white
+        $0.font = UIFont.systemFont(ofSize: 50)
+        $0.transform = CGAffineTransform(rotationAngle: 0.15)
+    }
+    
+    private let todayTopImageView = UIImageView().then {
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 3
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    // MARK: - Life Cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Layout
+    private func setLayout() {
+        [todayTopLabel, todayTopImageView].forEach {
+            contentView.addSubview($0)
+        }
+        
+        todayTopLabel.snp.makeConstraints {
+            $0.bottom.equalTo(todayTopImageView.snp.bottom)
+            $0.leading.equalToSuperview().offset(6)
+        }
+        
+        todayTopImageView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalTo(todayTopLabel.snp.trailing).offset(8)
+            $0.width.equalTo(98)
+            $0.height.equalTo(146)
+        }
+    }
+}
+
+extension TodaysTop20Cell {
+    func dataBind(_ itemData: TodaysTop20Content, itemRow: Int) {
+        todayTopLabel.text = "\(itemData.rank)"
+        todayTopImageView.image = itemData.poster
+    }
+}
