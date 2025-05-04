ea051cad56ddfac6dc637bb17b6ab938eabce1ca
hyeyeonie <shy020916@naver.com>
2025-05-05 03:24:12 +0900

    feat: '채널' UICollectionView 구현

--- assignment_week2_TVING/assignment_week2_TVING/Presentation/Main/Cell/ChannelCell.swift
+++ assignment_week2_TVING/assignment_week2_TVING/Presentation/Main/Cell/ChannelCell.swift
@@ -5,3 +5,45 @@
 //  Created by 신혜연 on 5/3/25.
 //
 
+import UIKit
+import Then
+
+class ChannelCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    static let identifier = "ChannelCell"
+    
+    private let backgroundContainerView = UIView()
+  
+    private let channelImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    // MARK: - Life Cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .black
+        
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Layout
+    private func setLayout() {
+        addSubview(channelImageView)
+        
+        channelImageView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().offset(12)
+            $0.width.equalTo(90)
+            $0.height.equalTo(45)
+        }
+    }
+    
+    func configure(_ content: ChannelContent) {
+        channelImageView.image = content.logoImage
+    }
+}
--- assignment_week2_TVING/assignment_week2_TVING/Presentation/Main/View/Channel.swift
+++ assignment_week2_TVING/assignment_week2_TVING/Presentation/Main/View/Channel.swift
@@ -5,3 +5,84 @@
 //  Created by 신혜연 on 5/3/25.
 //
 
+import UIKit
+import SnapKit
+
+class Channel: UIView, UICollectionViewDelegate {
+    
+    // MARK: - Properties
+    private var contentList = ChannelContent.dummy()
+//    private let itemData = ChannelContent.dummy()
+    
+    public let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 90, height: 45)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .black
+        collectionView.register(ChannelCell.self, forCellWithReuseIdentifier: ChannelCell.identifier)
+        return collectionView
+    }()
+    
+    // MARK: - Life Cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .black
+        
+        setLayout()
+        setDelegate()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Layout
+    private func setLayout() {
+        addSubview(collectionView)
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(50)
+        }
+    }
+                   
+    
+// MARK: - Delegate
+    private func setDelegate() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    private func setCollectionView() {
+        collectionView.register(ChannelCell.self, forCellWithReuseIdentifier: ChannelCell.identifier)
+    }
+}
+
+// MARK: - UICollectionView Delegate & DataSource
+extension Channel: UICollectionViewDataSource {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return contentList.count
+    }
+
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: ChannelCell.identifier,
+            for: indexPath
+        ) as? ChannelCell else {
+            return UICollectionViewCell()
+        }
+        cell.configure(contentList[indexPath.row])
+        return cell
+    }
+}
