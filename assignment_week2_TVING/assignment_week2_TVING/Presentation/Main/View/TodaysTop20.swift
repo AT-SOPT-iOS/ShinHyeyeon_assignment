ef4d3f31377b2a7dc63204a27b12354acaef073e
hyeyeonie <shy020916@naver.com>
2025-05-05 03:20:53 +0900

    feat: '오늘의 티빙 TOP 20' UICollectionView 구현

--- assignment_week2_TVING/assignment_week2_TVING/Presentation/Main/View/TodaysTop20.swift
+++ assignment_week2_TVING/assignment_week2_TVING/Presentation/Main/View/TodaysTop20.swift
@@ -5,3 +5,95 @@
 //  Created by 신혜연 on 5/3/25.
 //
 
+import UIKit
+import SnapKit
+
+class TodaysTop20: UIView {
+    
+    // MARK: - Properties
+    private let itemData = TodaysTop20Content.dummy()
+    
+    lazy var sectionTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "오늘의 티빙 TOP 20"
+        label.textColor = .white
+        label.font = .boldSystemFont(ofSize: 18)
+        return label
+    }()
+    
+    public let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 20
+        layout.itemSize = CGSize(width: 140, height: 146)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .black
+        collectionView.register(TodaysTop20Cell.self, forCellWithReuseIdentifier: TodaysTop20Cell.identifier)
+        return collectionView
+    }()
+    
+    // MARK: - Life Cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .black
+        
+        setLayout()
+        setDelegate()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Layout
+    private func setLayout() {
+        addSubview(sectionTitleLabel)
+        addSubview(collectionView)
+        
+        sectionTitleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().offset(12)
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(sectionTitleLabel.snp.bottom).offset(9)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(150)
+        }
+    }
+                   
+    
+// MARK: - Delegate
+    private func setDelegate() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    private func setCollectionView() {
+        collectionView.register(TodaysTop20Cell.self, forCellWithReuseIdentifier: TodaysTop20Cell.identifier)
+    }
+}
+
+// MARK: - UICollectionView Delegate & DataSource
+extension TodaysTop20: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 20)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TodaysTop20Cell.identifier, for: indexPath) as? TodaysTop20Cell else {
+            return UICollectionViewCell()
+        }
+        cell.dataBind(itemData[indexPath.item], itemRow: indexPath.item)
+        return cell
+    }
+}
