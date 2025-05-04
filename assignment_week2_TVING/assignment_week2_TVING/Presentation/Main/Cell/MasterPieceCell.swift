0397f80a87f0a4249873143c509a558f658ffb6b
hyeyeonie <shy020916@naver.com>
2025-05-05 03:25:13 +0900

    feat: 'PD의 인생작 TOP 5' UICollectionView 구현

--- assignment_week2_TVING/assignment_week2_TVING/Presentation/Main/Cell/MasterPieceCell.swift
+++ assignment_week2_TVING/assignment_week2_TVING/Presentation/Main/Cell/MasterPieceCell.swift
@@ -5,3 +5,47 @@
 //  Created by 신혜연 on 5/3/25.
 //
 
+import UIKit
+import Then
+
+class MasterpieceCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    static let identifier = "MasterPieceCell"
+    
+    private let backgroundContainerView = UIView()
+  
+    private let masterPieceImageView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+    }
+    
+    // MARK: - Life Cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .black
+        
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Layout
+    private func setLayout() {
+        contentView.addSubview(masterPieceImageView)
+        
+        masterPieceImageView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.width.equalTo(160)
+            $0.height.equalTo(90)
+        }
+    }
+}
+
+extension MasterpieceCell {
+    func dataBind(_ itemData: MasterpieceContent, itemRow: Int) {
+        masterPieceImageView.image = itemData.posterImage
+    }
+}
--- assignment_week2_TVING/assignment_week2_TVING/Presentation/Main/View/Masterpiece.swift
+++ assignment_week2_TVING/assignment_week2_TVING/Presentation/Main/View/Masterpiece.swift
@@ -5,3 +5,89 @@
 //  Created by 신혜연 on 5/3/25.
 //
 
+import UIKit
+import SnapKit
+
+class Masterpiece: UIView {
+    
+    // MARK: - Properties
+    private let itemData = MasterpieceContent.dummy()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "신혜연PD의 인생작 TOP 5"
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        return label
+    }()
+    
+    public let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 160, height: 90)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .black
+        collectionView.register(MasterpieceCell.self, forCellWithReuseIdentifier: MasterpieceCell.identifier)
+        return collectionView
+    }()
+    
+    // MARK: - Life Cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = .black
+        
+        setLayout()
+        setDelegate()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Layout
+    private func setLayout() {
+        addSubview(titleLabel)
+        addSubview(collectionView)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.leading.equalToSuperview().offset(8)
+        }
+        
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(13)
+            $0.horizontalEdges.equalToSuperview().offset(8)
+            $0.height.equalTo(100)
+        }
+    }
+                   
+    
+// MARK: - Delegate
+    private func setDelegate() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+    
+    private func setCollectionView() {
+        collectionView.register(MasterpieceCell.self, forCellWithReuseIdentifier: MasterpieceCell.identifier)
+    }
+}
+
+// MARK: - UICollectionView Delegate & DataSource
+extension Masterpiece: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MasterpieceCell.identifier, for: indexPath) as? MasterpieceCell else {
+            return UICollectionViewCell()
+        }
+        cell.dataBind(itemData[indexPath.item], itemRow: indexPath.item)
+        return cell
+    }
+}
